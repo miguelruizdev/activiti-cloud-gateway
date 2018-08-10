a7c6a1abac7ed5b767832a2837c06377d12cec26
Ryan Dawson <ryandawson@cantab.net>
2018-08-10 14:58:16 +0100

    implementation based on spring cloud gateway with sk8s discovery like blueprint (#2)

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # activiti-cloud-gateway
+[![Join the chat at https://gitter.im/Activiti/Activiti7](https://badges.gitter.im/Activiti/Activiti7.svg)](https://gitter.im/Activiti/Activiti7?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![Travis Status](https://travis-ci.org/Alfresco/alfresco-api-gateway.svg?branch=master)](https://travis-ci.org/Alfresco/alfresco-api-gateway) [![ASL 2.0](https://img.shields.io/hexpm/l/plug.svg)](https://github.com/Alfresco/alfresco-api-gateway/blob/master/LICENSE.txt) [![CLA assistant](https://cla-assistant.io/readme/badge/Alfresco/alfresco-api-gateway)](https://cla-assistant.io/Alfresco/alfresco-api-gateway) [![Docker Build Status](https://img.shields.io/docker/build/alfresco/alfresco-api-gateway.svg)](https://hub.docker.com/r/alfresco/alfresco-api-gateway/)
+
+Activiti Cloud Gateway
+
+This project is using the Spring Cloud Gateway project along the Spring Cloud Kubernetes project to create dynamic routes
+to the services registered inside a Kubernetes namespace. 
+
+The main goal of this project is to provide a single entrypoint for your client applications to interact with a set of backend services.
+
+Currently a spring.cloud.gateway class is overridden in the project due to these issues:
+
+https://github.com/spring-cloud/spring-cloud-gateway/issues/229
+and
+https://github.com/spring-cloud/spring-cloud-gateway/issues/314
+
+Currently discovery of routes only works on startup and if a new app is added then a POST to /application/gateway/refresh is necessary. It's not clear how to resolve this.
+
+Discovery is also limited to the current namespace.
---
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,60 @@
+package org.activiti.cloud.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,
+                              args);
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
+                                                                             DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient,
+                properties);
+    }
+
+    // see https://github.com/spring-cloud/spring-cloud-gateway/issues/229
+    // TODO: this may not be needed anymore as might now be able to configure with https://cloud.spring.io/spring-cloud-gateway/multi/multi__cors_configuration.html
+    @Bean
+    public CorsConfiguration corsConfiguration(RoutePredicateHandlerMapping routePredicateHandlerMapping) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PATCH).
+                forEach(m -> corsConfiguration.addAllowedMethod(m));
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("Authorization");
+        corsConfiguration.addAllowedHeader("authorization");
+        corsConfiguration.addAllowedHeader("Content-Type");
+        corsConfiguration.addAllowedHeader("Cache-Control");
+        corsConfiguration.addAllowedHeader("X-Requested-With");
+        corsConfiguration.addAllowedHeader("acccept");
+        corsConfiguration.addAllowedHeader("Origin");
+        corsConfiguration.addAllowedHeader("Access-Control-Request-Method");
+        corsConfiguration.addAllowedHeader("Access-Control-Request-Headers");
+        corsConfiguration.addAllowedHeader("X-CSRF-Token");
+        corsConfiguration.addExposedHeader("Access-Control-Allow-Origin");
+        corsConfiguration.addExposedHeader("Access-Control-Allow-Credentials");
+        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
+        return corsConfiguration;
+    }
+}
